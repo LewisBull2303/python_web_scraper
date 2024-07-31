ffa8ef3e446e568bb3f357dea4b2c47c647d1396
Lewis Bull <129514763+LewisBull2303@users.noreply.github.com>
2024-07-31 15:54:20 +0100

    Update scraper.py
    Decided to get the comments of who is hiring in the month of July from a website called hacker news. I have parsed in the link and it has printed out the number of elements that have the class comment on the website

--- scraper.py
+++ scraper.py
@@ -1,5 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
 def main():
-    print("Hello World")
+    url ="https://news.ycombinator.com/item?id=40846428"
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.content, "html.parser")
+    elements = soup.find_all(class_="comment")
+
+    print(f"Elements: {len(elements)}")
+
+    print(f"Scraping... {url}")
+    print(response)
+    print(response.content)
+
 
 if __name__ == "__main__":
     main()
